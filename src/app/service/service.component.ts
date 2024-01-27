72dab0bd732fc89cb8b36fdf170cbbcddb6d58ff
Heryakos Meles <heryakosmeles@gmail.com>
2024-01-27 00:06:34 -0900

    Adding header and footer inside the service componet

--- src/app/service/service.component.ts
+++ src/app/service/service.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-service',
   templateUrl: './service.component.html',
-  styleUrls: ['./service.component.css']
+  styleUrls: ['./service.component.css'],
 })
 export class ServiceComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
