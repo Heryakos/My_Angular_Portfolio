227c94dcbd7364b4c08aaebeb51fe380694596eb
Heryakos Meles <heryakosmeles@gmail.com>
2024-01-27 00:05:00 -0900

    Adding some content inside this pages

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -3,13 +3,48 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-about-us',
   templateUrl: './about-us.component.html',
-  styleUrls: ['./about-us.component.css']
+  styleUrls: ['./about-us.component.css'],
 })
 export class AboutUsComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  progressValues: any = {
+    html: 100,
+    css: 100,
+    javascript: 80,
+    typescript: 60,
+    angular: 80,
+    react: 60,
+  };
 
   ngOnInit(): void {
+    this.startProgressAnimation();
   }
 
+  startProgressAnimation(): void {
+    // Delay the start of each animation for a smooth effect
+    setTimeout(() => {
+      this.progressValues.html = 100;
+    }, 200);
+
+    setTimeout(() => {
+      this.progressValues.css = 100;
+    }, 400);
+
+    setTimeout(() => {
+      this.progressValues.javascript = 80;
+    }, 600);
+
+    setTimeout(() => {
+      this.progressValues.typescript = 60;
+    }, 800);
+
+    setTimeout(() => {
+      this.progressValues.angular = 80;
+    }, 1000);
+
+    setTimeout(() => {
+      this.progressValues.react = 60;
+    }, 1200);
+  }
 }
