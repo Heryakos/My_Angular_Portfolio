75e090a511c2a995ff9050540d7729c84ddcbf32
Heryakos Meles <heryakosmeles@gmail.com>
2024-01-30 05:37:18 -0900

    Fix home page

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,12 +1,43 @@
-import { Component, OnInit } from '@angular/core';
-import 'animate.css';
+import { Component, OnInit, HostListener } from '@angular/core';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css'],
+  animations: [
+    trigger('fadeIn', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(10px)' }),
+        animate(
+          '500ms ease-in-out',
+          style({ opacity: 1, transform: 'translateY(0)' })
+        ),
+      ]),
+    ]),
+  ],
 })
 export class HomePageComponent implements OnInit {
+  isAnimated = false;
+
+  @HostListener('window:scroll')
+  onWindowScroll() {
+    const scrollPosition =
+      window.pageYOffset ||
+      document.documentElement.scrollTop ||
+      document.body.scrollTop ||
+      0;
+    const windowHeight =
+      window.innerHeight ||
+      document.documentElement.clientHeight ||
+      document.body.clientHeight ||
+      0;
+
+    if (scrollPosition > windowHeight) {
+      this.isAnimated = true;
+    }
+  }
+
   constructor() {}
 
   ngOnInit(): void {}
