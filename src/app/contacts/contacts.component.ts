24159655689fc17ad3a95cda1aecf9386c7f7e30
Heryakos Meles <heryakosmeles@gmail.com>
2024-02-05 02:18:49 -0900

    adding some animation in the Contact us page just like the home page

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,11 +1,25 @@
 import { Component, HostListener, OnInit } from '@angular/core';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-contacts',
   templateUrl: './contacts.component.html',
   styleUrls: ['./contacts.component.css'],
+  animations: [
+    trigger('fadeIn', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(10px)' }),
+        animate(
+          '500ms ease-in-out',
+          style({ opacity: 1, transform: 'translateY(0)' })
+        ),
+      ]),
+    ]),
+  ],
 })
 export class ContactsComponent implements OnInit {
+  isAnimated = false;
+
   constructor() {}
 
   ngOnInit(): void {}
